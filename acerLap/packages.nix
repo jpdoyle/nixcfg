7b3e58c2525986676d1e05451cc6f80d327bfd6d
Joe Doyle <joethedoyle@gmail.com>
2017-01-15 13:57:08 -0500

    New packages for acerlap

--- acerLap/packages.nix
+++ acerLap/packages.nix
@@ -1,17 +1,33 @@
 { config, pkgs, ... }:
 
 {
-  # List packages installed in system profile. To search by name, run:
-  # $ nix-env -qaP | grep wget
-  environment.systemPackages = with pkgs; [
-    steam
-    stack
-    j
-    sfml
-    cargo
-    gnumeric
-    bashmount
-  ];
+    # List packages installed in system profile. To search by name, run:
+    # $ nix-env -qaP | grep wget
+    environment.systemPackages = with pkgs; [
+        steam
+        stack
+        j
+        sfml
+        cargo
+        gnumeric
+        bashmount
+        openssl_1_1_0
+        openssl
+        arduino
+
+        jre
+        openjdk
+        ant
+        avrgcclibc
+        avrdude
+
+        dfu-util
+        xfsprogs
+
+        gksu
+        screen
+        torbrowser
+    ];
 
 }
 
